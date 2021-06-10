c6c0089d5b4c56005067bcd1aea39f63d44eca57
nizzola <marcio@nizzola.com.br>
2021-06-10 20:54:17 -0300

    Add project files.

---
+++ TaskPocExample/JobToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskPocExample
+{
+    public class JobToDo
+    {
+        
+        public async void DoWork(ConcurrentBag<(string,string)> logs, string jobName, int interval)
+        {
+            for(int x = 1; x < 10; x ++ )
+            {
+                Thread.Sleep(interval);
+                logs.Add( new (jobName , DateTime.Now.ToString("mm:ss:ffff")) );
+            }
+        }
+
+    }
+}
---
+++ TaskPocExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskPocExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start job execution");
+            var lista = new ConcurrentBag<(string,string)>();
+            
+            List<Task> tasks = new List<Task>();
+
+            tasks.Add(Task.Factory.StartNew(() => new JobToDo().DoWork(lista, "JOB 1", 50) ));
+            tasks.Add(Task.Factory.StartNew(() => new JobToDo().DoWork(lista, "JOB 2", 20)));
+            tasks.Add(Task.Factory.StartNew(() => new JobToDo().DoWork(lista, "JOB 3", 10)));
+
+            var taskArray = tasks.ToArray();
+            
+            Task.WaitAll(taskArray);
+
+            foreach (var item in lista.OrderBy(a => a.Item2) )
+            {
+                Console.WriteLine(item.Item1 + " - " + item.Item2);
+            }
+
+        }
+    }
+}
